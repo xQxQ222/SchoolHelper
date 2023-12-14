6a60729c36b284e192b349754a34de9d70187699
xQxQ222 <nerounxqxq@gmail.com>
2023-12-15 02:12:04 +0400

    Добавлен класс User

---
+++ WinFormsApp1/User.cs
@@ -0,0 +1,16 @@
+﻿namespace WinFormsApp1
+{
+    class User
+    {
+        public static User Current { get; set; }=new User();
+        public int _id;
+        public string _login;
+        public string _password;
+        public string _name;
+        public string _surename;
+        public string _patronymic;
+        public DateTime _Date;
+        public string _email;
+        public string _status;
+    }
+}
