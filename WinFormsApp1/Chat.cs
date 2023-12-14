e60cca61e8bc33b2842ee5e7e5b3dce51f97b9ef
Ivan <nerounxqxq@gmail.com>
2023-12-14 22:56:21 +0400

    Добавлена логика класса Chat. Добавлены комментарии к методам

--- WinFormsApp1/Chat.cs
+++ WinFormsApp1/Chat.cs
@@ -10,9 +10,33 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        private void Chat_Load(object sender, EventArgs e)
+        private void BackButton_Click(object sender, EventArgs e)//Метод, который при нажатии на кнопку "Назад" перекидывает пользователя на окно "Menu"
         {
-            MessageBox.Show("Hello");
+            var form = new Menu();
+            form.Show();
+            this.Close();
         }
+
+        private void ExitButton_Click(object sender, EventArgs e)//Метод закрытия приложения при нажатии на крестик
+        {
+            Application.Exit();
+        }
+
+        private void Chat_Load(object sender, EventArgs e)//Метод, вызываемый при загрузке формы. Добавляет в ComboBox имена доступных получателей
+        {
+            var recip = ReadFromDB.GetRecipients();//метод описан в классе ReadFromDB
+            comboBox1.DataSource = recip.Item1;
+        }
+
+        private void SendMessageButton_Click(object sender, EventArgs e)//Метод, позволяющий пользователю отправить письмо во внутреннем чате программы другому пользователю
+        {
+            var recip = ReadFromDB.GetRecipients();
+
+            WriteToDB.AddNewMessage(recip, comboBox1.Text, richTextBox1.Text);
+
+            MessageBox.Show("Сообщение успешно отправлено");
+            richTextBox1.Clear();
+        }
+
     }
 }
