e1a5847f420e865bb38a8b79a8467b281036426e
Zhenya <tozhe2004@gmail.com>
2023-12-14 16:38:15 +0400

    Добавлена реализация методов класса StartScreen

--- WinFormsApp1/StartScreen.cs
+++ WinFormsApp1/StartScreen.cs
@@ -7,5 +7,43 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void RegistrationButton_Click(object sender, EventArgs e)
+        {
+            var regForm = new RegistrationForm();
+            regForm.Show();
+            Hide();
+        }
+
+        
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            var logForm = new LoginForm();
+            logForm.Show();
+            Hide();
+        }
+
+        
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        Point lastPoint;
+        private void StartScreen_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void StartScreen_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
     }
 }
