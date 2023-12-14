311e50f90505cfc62e73e1d3527fff662d75061c
Ivan <nerounxqxq@gmail.com>
2023-12-14 23:03:09 +0400

    Добавлены классы ImageUploader и ImageRetriever для работы с загрузкой пользовательских изображений. Добавлена логика в класс Profile

--- WinFormsApp1/Profile.cs
+++ WinFormsApp1/Profile.cs
@@ -16,5 +16,46 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Profile_Load(object sender, EventArgs e)
+        {
+            name.Text = User.Current._name;
+            surename.Text = User.Current._surename;
+            patronymic.Text = User.Current._patronymic;
+            status.Text = User.Current._status;
+            birthDate.Text = User.Current._Date.ToShortDateString();
+            email.Text = User.Current._email;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var form = new Menu();
+            form.Show();
+            this.Close();
+        }
     }
 }
