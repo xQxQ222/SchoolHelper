358a14e6d84ea58c68c13e7d88ba0b2fb777f017
Anastasiya <irodenkon@gmail.com>
2023-12-14 18:09:18 +0400

    Добавлены комментарии в класс DB

--- WinFormsApp1/DB.cs
+++ WinFormsApp1/DB.cs
@@ -2,25 +2,25 @@
 
 namespace WinFormsApp1
 {
-    class DB
+    class DB//класс для работы с БД
     {
-        MySqlConnection connect = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=schoolhelper");
+        MySqlConnection connect = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=schoolhelper");//Подключение к базе данных MySql
 
-        public void openConnection()
+        public void openConnection()//Метод для открытия соединения с БД
         {
             if (connect.State == System.Data.ConnectionState.Closed)
             {
                 connect.Open();
             }
         }
-        public void closeConnection()
+        public void closeConnection()//метод для закрытия соединения с БД
         {
             if (connect.State == System.Data.ConnectionState.Open)
             {
                 connect.Close();
             }
         }
-        public MySqlConnection getConnection()
+        public MySqlConnection getConnection()//Метод, возвращающий переменную для соединения с БД
         {
             return connect;
         }
