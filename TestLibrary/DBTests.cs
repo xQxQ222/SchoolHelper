577945a343ebb4980c716836f67d7a30dbe1438a
xQxQ222 <nerounxqxq@gmail.com>
2023-12-25 02:02:22 +0400

    Добавлен тест, остальное - завтра

---
+++ TestLibrary/DBTests.cs
@@ -0,0 +1,19 @@
+﻿using WinFormsApp1;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+namespace TestLibrary
+{
+    [TestFixture]
+    public class DBTests
+    {
+        [Test]
+        public void RegAndConfirmUserCheck()
+        {
+            var user=new User("login","password","Джон","Лобанов","Иванович",DateTime.Now,"Ученик","11А","lol123@gmail.com");
+            var check=ChangeDBData.RegisterANewUser(user);
+            ChangeDBData.DeleteUser(user,"awaitingconfirmation");
+            ClassicAssert.AreEqual(true, check);
+            
+        }
+    }
+}
